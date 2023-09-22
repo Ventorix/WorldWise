20352b33b171ecb9f77f1afa67b278c6cb32ab31
1Fenix7 <userpk66@gmail.com>
2023-09-22 23:25:06 +0300

    feat: add Nested Routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
@@ -10,11 +10,16 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Homepage />} />
+				<Route index element={<Homepage />} />
 				<Route path='product' element={<Product />} />
 				<Route path='pricing' element={<Pricing />} />
 				<Route path='login' element={<Login />} />
-				<Route path='app' element={<AppLayout />} />
+				<Route path='app' element={<AppLayout />}>
+					<Route index element={<Navigate replace to='cities' />} />
+					<Route path='cities' element={<p>Cities</p>} />
+					<Route path='countries' element={<p>Countries</p>} />
+					<Route path='form' element={<p>form</p>} />
+				</Route>
 				<Route path='*' element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
