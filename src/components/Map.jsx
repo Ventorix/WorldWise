dbd8e16494f0c7ed4115df1370411cbf0d57ca11
1Fenix7 <userpk66@gmail.com>
2023-09-24 23:44:06 +0300

    feat: add interactive map to app using Leaflet API

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,29 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import styles from './Map.module.css';
+import { useState } from 'react';
 
 function Map() {
 	const [searchParams, setSearchParams] = useSearchParams();
+	const [mapPosition, setMapPosition] = useState([40, 0]);
+
 	const navigate = useNavigate();
 	const lat = searchParams.get('lat');
 	const lng = searchParams.get('lng');
 
 	return (
-		<div className={styles.mapContainer} onClick={() => navigate('form')}>
-			<h1>{lat && lng ? `Position: ${lat}, ${lng}` : 'Not data'}</h1>
+		<div className={styles.mapContainer}>
+			<MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+				/>
+				<Marker position={mapPosition}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
 		</div>
 	);
 }
